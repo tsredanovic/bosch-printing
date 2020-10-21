56da1685e905de1c526dd1e0f1c60ac80eba0624
Toni Sredanović <toni@krakensystems.co>
2020-10-21 13:51:11 +0200

    new message format and centering

--- printing.py
+++ printing.py
@@ -1,28 +1,34 @@
 import asyncio
+import json
 
 import websockets
 from escpos.printer import Serial
 
 """ 9600 Baud, 8N1, Flow Control Enabled """
-p = Serial(devfile='/dev/ttyUSB0',
-           baudrate=19200,
-           bytesize=8,
-           parity='N',
-           stopbits=1,
-           timeout=1.00,
-           dsrdtr=True)
+printer = Serial(devfile='/dev/ttyUSB0',
+                 baudrate=19200,
+                 bytesize=8,
+                 parity='N',
+                 stopbits=1,
+                 timeout=1.00,
+                 dsrdtr=True)
 
 
 async def print_content(websocket, path):
     # Receive
     content = await websocket.recv()
+    content = json.loads(content)
     print('GOT: {}'.format(content))
 
-    # Print
-    p.text("Tole je vas kod za narocilo\n")
-    p.qr(str(content), size=8, native=True)
-    p.cut()
-    print('PRINTING: {}'.format(content))
+    # Align text
+    printer.set(align='center')
+    # Print items part
+    for item in content['items']:
+        printer.text('{} {}\n'.format(item['ean'], item['quantity']))
+    # Print QR part
+    printer.text('Tole je vas kod za narocilo\n')
+    printer.qr(content=str(content['number']), size=8, native=False, center=True)
+    printer.cut()
 
 start_server = websockets.serve(print_content, "127.0.0.1", 5555)
 
