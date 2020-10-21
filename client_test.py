be3050cf8bc217d714f41187c9071dab756bd576
Toni Sredanović <toni@krakensystems.co>
2020-10-21 13:49:40 +0200

    new format and centering in dummy printing

--- client_test.py
+++ client_test.py
@@ -1,16 +1,32 @@
 import asyncio
+import json
+
 import websockets
 
 
 async def hello():
     uri = "ws://localhost:5555"
     async with websockets.connect(uri) as websocket:
-        name = input('What to send: ')
+        payload = {
+            "number": 69,
+            "items": [
+                {
+                    "ean": 1,
+                    "quantity": 3
+                },
+                {
+                    "ean": 2,
+                    "quantity": 1
+                },
+                {
+                    "ean": 3,
+                    "quantity": 8
+                }
+            ]
+        }
+        payload_json = json.dumps(payload)
 
-        await websocket.send(name)
-        print('Sent: {}'.format(name))
-
-        greeting = await websocket.recv()
-        print('Got: {}'.format(greeting))
+        await websocket.send(payload_json)
+        print('Sent: {}'.format(payload_json))
 
 asyncio.get_event_loop().run_until_complete(hello())
---
+++ dummy_printing.py
@@ -0,0 +1,34 @@
+import asyncio
+import json
+
+import websockets
+from escpos.printer import Dummy
+
+""" Dummy """
+my_printer_def = Dummy()
+my_printer_def.profile.profile_data['media']['width']['mm'] = 700
+my_printer_def.profile.profile_data['media']['width']['pixels'] = 700
+
+
+async def print_content(websocket, path):
+    # Receive
+    content = await websocket.recv()
+    content = json.loads(content)
+    print('GOT: {}'.format(content))
+
+    # Align text
+    my_printer_def.set(align='center')
+    # Print items part
+    for item in content['items']:
+        my_printer_def.text('{} {}\n'.format(item['ean'], item['quantity']))
+    # Print QR part
+    my_printer_def.text('Tole je vas kod za narocilo\n')
+    my_printer_def.qr(content=str(content['number']), size=8, native=False, center=True)
+    my_printer_def.cut()
+    with open('output.prn', 'wb') as file:
+        file.write(my_printer_def.output)
+
+start_server = websockets.serve(print_content, "127.0.0.1", 5555)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
