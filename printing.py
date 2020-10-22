38f8dbcc9668ff3453e77a71d0ced0b0c684597b
Toni Sredanović <toni@krakensystems.co>
2020-10-22 11:22:51 +0200

    fix alignment and newlines

--- printing.py
+++ printing.py
@@ -21,23 +21,22 @@ async def print_content(websocket, path):
     print('GOT: {}'.format(content))
 
     # Align text
-    printer.set(align='center')
+    printer.set(align='center', bold=False)
     # Print datetime part
     printer.text('Datum: {}\n'.format(content['datetime']))
     printer.ln(count=1)
     # Print number part
     printer.text('Številka naročila: ')
-    printer.set(bold=True)
+    printer.set(align='center', bold=True)
     printer.text('{}\n'.format(content['number']))
-    printer.set(bold=False)
+    printer.set(align='center', bold=False)
     printer.ln(count=2)
     # Print items part
     for item in content['items']:
         printer.text('{} ({}) - {}\n'.format(item['name'], item['ean'], item['quantity']))
-    printer.ln(count=4)
+    printer.ln(count=3)
     # Print QR part
     printer.text('Tole je vas kod za narocilo\n')
-    printer.ln(count=1)
     printer.qr(content=str(content['number']), size=8, native=False, center=True)
     printer.cut()
 
